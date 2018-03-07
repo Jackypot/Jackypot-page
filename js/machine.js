cc177ec01a4393f00f0a242e06bc0037bef18ff4
Jesus Glez <chuy_2808@hotmail.com>
2018-03-07 16:53:09 -0600

    Add comments Machine.js

--- js/machine.js
+++ js/machine.js
@@ -1,38 +1,60 @@
+// **********************************************************
+// Obtencion del tamaño de Viewport en relacion a Width
+// **********************************************************
+var vw;
 vw = window.innerWidth;
+window.addEventListener("resize", function(){ vw = window.innerWidth; });
 
 // **********************************************************
 // Creacion de datos a mostrar
 // **********************************************************
+var contador = 0;
+var intervalo_datos, intervalo_giros, respuesta, ethDls, pote, intervalo_valorPote;
 
-// End Point
-// ********************************
+//FUNCION: Get respuesta de EndPoint
 function obtencion_datos_endpoint(){
     fetch('https://jackynet.eu-4.evennode.com/table')
     .then(function(response){if (!response.ok) {throw Error(response.statusText);} return response.json();})
     .then(function(resp) {
-        respuesta = resp.dataTable;//Guadamos los datos
-        intervalo_datos = setInterval(function(){manager(respuesta)}, 3000);//inicio de creacion de la tabla
 
+        //Guardamos la respuesta
+        respuesta = resp.dataTable;
+        //INTERVALO: Mostrar datos en seccion "BETS"
+        intervalo_datos = setInterval(function(){manager(respuesta)}, 3000);
+
+        //Numeros aleatorios con el cual representar el primer giro al inicio de la pagina
         let num = Math.floor((Math.random() * 9) + 1);
+        //INIT FUNCION: Girar al entrar a la pag. con resultado de triples
+        //Parametros Resultado, Resultado, Resultado, Profit, Clase
         playGiro(num, num, num, false, false);
 
-        mostrar_pote()
-        intervalo_valorPote = setInterval(mostrar_pote, 300000);//cambiar el valor de ether a dolares cada 5min
-        })
+        //INIT FUNCION: Mostrar valor del pote
+        mostrar_pote();
+        //INTERVALO: Mostrar el valor del pote Ethereum/USD
+        intervalo_valorPote = setInterval(mostrar_pote, 300000);
+    })
     .catch(function(error) {console.log('Looks like there was a problem: \n', error);});
 }
+
+//INIT FUNCION: EndPoint
 obtencion_datos_endpoint();
 
-var contador = 0;
-var intervalo_datos, intervalo_giros, respuesta, ethDls, pote, intervalo_valorPote;
-
-// Funcion mostrar valor del pote
-// Correcto
+// **********************************************************
+// FUNCION: Mostrar valor del pote Ethereum/USD
+// **********************************************************
+//Checa el estado en la que se visualiza el valor del pote
+//TRUE --> Ethereum
+//FALSE --> USD
 var estado_pote = true;
+
+//FUNCION
 function mostrar_pote(){
     if (estado_pote) {
+        //Guardamos el valor del pote obtenido de la respuesta
         pote = respuesta[respuesta.length - 1].potValue;
+        //Mostramos el valor del pote
         document.getElementById("pote").innerHTML = pote.toFixed(2) +" ETH";
+        //Cambiamos el estado a false
         estado_pote = false;
     }else {
         let aux = pote * ethDls;
@@ -41,9 +63,10 @@ function mostrar_pote(){
     }
 }
 
-// Funcion obtener el valor ETH/USD y conversion
-// Correcto - Usa Fetch externo [cryptocompare]
-function getValorDLS() {
+// **********************************************************
+// FUNCION: Obtener valor del Ethereum [Api CrytoCompare]
+// **********************************************************
+function obtener_valor_ethereum() {
 	var url = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD';
 	fetch(url)
 	.then(function(response){if (!response.ok) {throw Error(response.statusText);} return response.json();})
@@ -51,50 +74,53 @@ function getValorDLS() {
 	.catch(function(error){console.log('Parece que hubo un error: ' + error);});
 }
 
-getValorDLS();
-setInterval(getValorDLS, 1800000)//peticion de valor del ether 30min.
+//INIT FUNCION
+obtener_valor_ethereum();
+// INTERVALO: Obtener el valor del Ethereum cada 30min.
+setInterval(obtener_valor_ethereum, 1800000);
 
-// Calcular Profit
-// ********************************
+
+// **********************************************************
+// FUNCION: Calcular valor de profit y su clase
+// **********************************************************
+//Parametros: Resultado1, Resultado2, Resultado3, Apuesta
 function calcular_profit(a, b, c, bet){
     let cambioNum = ["", 1, 1, 1, 2, 2, 2, 3, 3, 3];
 	let profit, color;
 	bet = (bet / 1000000000000000000);
+
+    //Comprobacion de triples
 	if ((cambioNum[a] == cambioNum[b]) && (cambioNum[b] == cambioNum[c])) {
 		if (cambioNum[a] == 1) {profit = "150%"; /*(bet * 1.5).toFixed(3);*/}
 		if (cambioNum[a] == 2) {profit = "200%";/*(bet * 2).toFixed(3);*/}
-		if (cambioNum[a] == 3) {profit = "300%"/*(bet * 3).toFixed(3);*/}
+		if (cambioNum[a] == 3) {profit = "300%";/*(bet * 3).toFixed(3);*/}
 		color = 'verde';
-	}else if ((cambioNum[a] == cambioNum[b]) || (cambioNum[b] == cambioNum[c])) {color = 'verde'; profit = "110%"/*(bet * 1.1).toFixed(3);*/}
+	}
+    //Comprobacion de dobles
+    else if ((cambioNum[a] == cambioNum[b]) || (cambioNum[b] == cambioNum[c])) {color = 'verde'; profit = "110%"/*(bet * 1.1).toFixed(3);*/}
+    //Perdio
 	else {color = 'rojo'; profit = "0%" /*bet.toFixed(3);*/}
 
 	return [profit, color];
 }
 
+// **********************************************************
+// FUNCION: Mostrar datos de almacenados en respuesta [Seccion "BETS"]
+// **********************************************************
 var manager = (datos) => {
-    if (contador < datos.length - 2) {
-        contador ++
-    }
-    else {
-        contador = 0;
+    if (contador < datos.length - 2) { contador ++ }
+    else { contador = 0; }
 
-    }
-
+    //Dependiendo del vw, seleccionamos donde mostrar los datos
     let div_correspondiente;
-    if (vw < 481) {div_correspondiente = "datos-m";}
-    else {div_correspondiente = "datos";}
+    if (vw < 481) {div_correspondiente = "datos-m";} //Ver. telefonos < 480px
+    else {div_correspondiente = "datos";}//Ver. tablet o superior > 481px
 
 
     let padre = document.getElementById(div_correspondiente);
-    if ((padre.firstChild) && (padre.childNodes.length < 2)) {
-        add(padre, true, datos);
-    }
-    else if (!padre.firstChild) {
-        add(padre, true, datos);
-    }
-    else if ((padre.childNodes.length > 1) && (padre.childNodes.length < 10)) {
-        add(padre, true, datos);
-    }
+    if ((padre.firstChild) && (padre.childNodes.length < 2)) { add(padre, true, datos); }
+    else if (!padre.firstChild) { add(padre, true, datos); }
+    else if ((padre.childNodes.length > 1) && (padre.childNodes.length < 10)) { add(padre, true, datos); }
     else if (padre.childNodes.length == 10) {
         less(padre);
         add(padre, true, datos);
@@ -102,8 +128,6 @@ var manager = (datos) => {
 }
 
 var add = (_padre, numero, datos) => {
-    // let id => [arreglo]
-
 
     let contenedor = document.createElement("div");
     let contenedor_resultado = document.createElement("div");
@@ -148,7 +172,6 @@ var add = (_padre, numero, datos) => {
         contenedor.appendChild(contenedor_resultado);
     }
     else {
-        console.log(numero);
         let value_profit = calcular_profit(datos[numero].Resultado.charAt(0), datos[numero].Resultado.charAt(2), datos[numero].Resultado.charAt(4), datos[numero].Valor)
         let img = new Image();
         img.src = blockies.toDataUrl(datos[numero].Cliente);
@@ -181,36 +204,36 @@ var add = (_padre, numero, datos) => {
     _padre.insertBefore(contenedor, _padre.firstChild);
 }
 
-var less = (_padre) => {
-    _padre.removeChild(_padre.lastChild);
-}
+var less = (_padre) => { _padre.removeChild(_padre.lastChild); }
 
 var reordenar = (datos) => {
+
+    //Dependiendo del vw, seleccionamos donde mostrar los datos
     let div_correspondiente;
-    if (vw < 481) {div_correspondiente = "datos-m";}
-    else {div_correspondiente = "datos";}
+    if (vw < 481) {div_correspondiente = "datos-m";} //Ver. telefonos < 480px
+    else {div_correspondiente = "datos";}//Ver. tablet o superior > 481px
 
     let padre = document.getElementById(div_correspondiente);
     contador = 0;
 
-    while (padre.firstChild) {
-        padre.removeChild(padre.firstChild);
-    }
+    while (padre.firstChild) { padre.removeChild(padre.firstChild); }
 
     while (contador < 8) {
         if (contador > 4) {
             let aux = (contador - (respuesta.length-3))
             add(padre,aux, datos)
-        }else {
-            add(padre, contador, datos);
         }
+        else { add(padre, contador, datos); }
         contador ++;
     }
 }
+
 // **********************************************************
-// Maquina
+// FUNCION: Reproducir el resultado de un dato [Seccion "BETS"]
 // **********************************************************
+
 var estado_remover_clase = true;
+//Parametros: Id del div
 function datos_girar_maquina(ev){
     let a, b, c;
     let resultado, classe, valor_profit;
@@ -232,19 +255,22 @@ function datos_girar_maquina(ev){
         estado_remover_clase = false;
     }
 
-
+    //INIT FUNCION: Animacion de giros
+    //Parametros: resultado1, resultado2, resultado3, profit, clase
     playGiro(a, b, c, valor_profit, classe)
 }
 
-var vw;
-window.addEventListener("resize", function(){
-    vw = window.innerWidth;
-});
-
-// Giramos la maquina
+// **********************************************************
+// FUNCION PRINCIPAL: Animacion de giros
+// **********************************************************
+//Estado de la maquina
+//FALSE --> No esta girando
+//TRUE --> Esta mostrando un resultado
 var maquina_girando = false;
 function playGiro(a, b, c, profit, classe){
+    //Comprueba el estado de la maquina
     if (maquina_girando == false) {
+        //Cambiamos su estado
         maquina_girando = true;
 
 	    // Variables pantallas de ordenador
@@ -255,9 +281,13 @@ function playGiro(a, b, c, profit, classe){
 	    var posZ = 247;
         let translado = 60
 
-        let imgOne = Math.floor((Math.random() * 3) + 1);
-        let imgtwo = Math.floor((Math.random() * 3) + 1);
-        let imgthree = Math.floor((Math.random() * 3) + 1);
+        function imagenesRandom(){
+            let imgOne = Math.floor((Math.random() * 3) + 1);
+            let imgtwo = Math.floor((Math.random() * 3) + 1);
+            let imgthree = Math.floor((Math.random() * 3) + 1);
+
+            return [imgOne, imgtwo, imgthree];
+        }
 
 	    // Para las diferentes pantallas
 	    if (vw <= 480){size = 50; posX = 15; posY = 79; posZ = 140; translado = 26;    }
@@ -281,82 +311,77 @@ function playGiro(a, b, c, profit, classe){
 
 	    // Empezamos el primer recorrido
 	    setTimeout(function(){
-	    	x = s.image("assets/game/slot/"+imgOne+".svg", posX, 10, size, size);
+            let img_random = imagenesRandom();
+
+	    	x = s.image("assets/game/slot/"+img_random[0]+".svg", posX, 10, size, size);
 	    	x.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-	    	y = s.image("assets/game/slot/"+imgtwo+".svg", posY, 10, size, size);
+	    	y = s.image("assets/game/slot/"+img_random[1]+".svg", posY, 10, size, size);
 	    	y.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-	    	z = s.image("assets/game/slot/"+imgthree+".svg", posZ, 10, size, size);
+	    	z = s.image("assets/game/slot/"+img_random[2]+".svg", posZ, 10, size, size);
 	    	z.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-	    	imgOne = Math.floor((Math.random() * 3) + 1);
-	    	imgtwo = Math.floor((Math.random() * 3) + 1);
-	    	imgthree = Math.floor((Math.random() * 3) + 1);
 	    }, 100);
 
         setTimeout(function(){
             s.clear();
-            x = s.image("assets/game/slot/"+imgOne+".svg", posX, 10, size, size);
+            let img_random = imagenesRandom();
+
+            x = s.image("assets/game/slot/"+img_random[0]+".svg", posX, 10, size, size);
             x.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            y = s.image("assets/game/slot/"+imgtwo+".svg", posY, 10, size, size);
+            y = s.image("assets/game/slot/"+img_random[1]+".svg", posY, 10, size, size);
             y.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            z = s.image("assets/game/slot/"+imgthree+".svg", posZ, 10, size, size);
+            z = s.image("assets/game/slot/"+img_random[2]+".svg", posZ, 10, size, size);
             z.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            imgOne = Math.floor((Math.random() * 3) + 1);
-            imgtwo = Math.floor((Math.random() * 3) + 1);
-            imgthree = Math.floor((Math.random() * 3) + 1);
-        }, 400);datos_girar_maquina
+        }, 400);
 
         setTimeout(function(){
             s.clear();
-            x = s.image("assets/game/slot/"+imgOne+".svg", posX, 10, size, size);
+            let img_random = imagenesRandom();
+
+            x = s.image("assets/game/slot/"+img_random[0]+".svg", posX, 10, size, size);
             x.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            y = s.image("assets/game/slot/"+imgtwo+".svg", posY, 10, size, size);
+            y = s.image("assets/game/slot/"+img_random[1]+".svg", posY, 10, size, size);
             y.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            z = s.image("assets/game/slot/"+imgthree+".svg", posZ, 10, size, size);
+            z = s.image("assets/game/slot/"+img_random[2]+".svg", posZ, 10, size, size);
             z.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            imgOne = Math.floor((Math.random() * 3) + 1);
-            imgtwo = Math.floor((Math.random() * 3) + 1);
-            imgthree = Math.floor((Math.random() * 3) + 1);
         }, 700);
 
         setTimeout(function(){
             s.clear();
-            x = s.image("assets/game/slot/"+imgOne+".svg", posX, 10, size, size);
+            let img_random = imagenesRandom();
+
+            x = s.image("assets/game/slot/"+img_random[0]+".svg", posX, 10, size, size);
             x.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            y = s.image("assets/game/slot/"+imgtwo+".svg", posY, 10, size, size);
+            y = s.image("assets/game/slot/"+img_random[1]+".svg", posY, 10, size, size);
             y.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            z = s.image("assets/game/slot/"+imgthree+".svg", posZ, 10, size, size);
+            z = s.image("assets/game/slot/"+img_random[2]+".svg", posZ, 10, size, size);
             z.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            imgOne = Math.floor((Math.random() * 3) + 1);
-            imgtwo = Math.floor((Math.random() * 3) + 1);
-            imgthree = Math.floor((Math.random() * 3) + 1);
         }, 1000);
 
         setTimeout(function(){
             s.clear();
-            x = s.image("assets/game/slot/"+imgOne+".svg", posX, 10, size, size);
+            let img_random = imagenesRandom();
+
+            x = s.image("assets/game/slot/"+img_random[0]+".svg", posX, 10, size, size);
             x.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            y = s.image("assets/game/slot/"+imgtwo+".svg", posY, 10, size, size);
+            y = s.image("assets/game/slot/"+img_random[1]+".svg", posY, 10, size, size);
             y.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            z = s.image("assets/game/slot/"+imgthree+".svg", posZ, 10, size, size);
+            z = s.image("assets/game/slot/"+img_random[2]+".svg", posZ, 10, size, size);
             z.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            imgOne = Math.floor((Math.random() * 3) + 1);
-            imgtwo = Math.floor((Math.random() * 3) + 1);
-            imgthree = Math.floor((Math.random() * 3) + 1);
         }, 1300);
 
         // Resultado uno
@@ -370,33 +395,34 @@ function playGiro(a, b, c, profit, classe){
         setTimeout(function(){
             y.remove();
 	    	z.remove();
-            y = s.image("assets/game/slot/"+imgtwo+".svg", posY, 10, size, size);
+            let img_random = imagenesRandom();
+
+            y = s.image("assets/game/slot/"+img_random[1]+".svg", posY, 10, size, size);
             y.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            z = s.image("assets/game/slot/"+imgthree+".svg", posZ, 10, size, size);
+            z = s.image("assets/game/slot/"+img_random[2]+".svg", posZ, 10, size, size);
             z.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            imgtwo = Math.floor((Math.random() * 3) + 1);
-            imgthree = Math.floor((Math.random() * 3) + 1);
         }, 1600);
 
         setTimeout(function(){
             y.remove();
 	    	z.remove();
-            y = s.image("assets/game/slot/"+imgtwo+".svg", posY, 10, size, size);
+            let img_random = imagenesRandom();
+
+            y = s.image("assets/game/slot/"+img_random[1]+".svg", posY, 10, size, size);
             y.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            z = s.image("assets/game/slot/"+imgthree+".svg", posZ, 10, size, size);
+            z = s.image("assets/game/slot/"+img_random[2]+".svg", posZ, 10, size, size);
             z.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            imgtwo = Math.floor((Math.random() * 3) + 1);
-            imgthree = Math.floor((Math.random() * 3) + 1);
         }, 1900);
 
         // Segundo resultado
         setTimeout(function(){
             y.remove();
 	    	z.remove();
+
             y = s.image("assets/game/slot/"+b+".svg", posY, 10, size, size);
             y.animate({ transform: 'translate(0 '+translado+')' }, 250, mina.linear )
         }, 2200);
@@ -404,18 +430,20 @@ function playGiro(a, b, c, profit, classe){
         // Tercera Vuelta
         setTimeout(function(){
             z.remove();
-            z = s.image("assets/game/slot/"+imgthree+".svg", posZ, 10, size, size);
+            let img_random = imagenesRandom();
+
+            z = s.image("assets/game/slot/"+img_random[2]+".svg", posZ, 10, size, size);
             z.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            imgthree = Math.floor((Math.random() * 3) + 1);
         }, 2200);
 
         setTimeout(function(){
             z.remove();
-            z = s.image("assets/game/slot/"+imgthree+".svg", posZ, 10, size, size);
+            let img_random = imagenesRandom();
+
+            z = s.image("assets/game/slot/"+img_random[2]+".svg", posZ, 10, size, size);
             z.animate({ transform: 'translate(0 250)' }, 250, mina.linear )
 
-            imgthree = Math.floor((Math.random() * 3) + 1);
         }, 2500);
 
         // Tercer resultado
@@ -425,6 +453,8 @@ function playGiro(a, b, c, profit, classe){
             z.animate({ transform: 'translate(0 '+translado+')' }, 250, mina.linear )
             maquina_girando = false;
         }, 2800);
+
+
 
         // Impresion de profit al finalizar giro
         setTimeout(function(){
